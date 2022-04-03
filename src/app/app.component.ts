a59b3bd26abc3eac50ffc31265b9c3d918e504d5
damiaofranca <francadamiao202@gmail.com>
2022-04-03 01:21:35 -0300

    add resources, photos, resize page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+
+  }
+
+  // go to url in browser in another tab
+  goToUrl(url: string) {
+    window.open(url, '_blank');
+  }
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({ behavior: "smooth" });
+
+  }
 }
